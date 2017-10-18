d4e40cd91502f4d2fb1b338e50b5282ff43e0336
Emimini <emeline.buoy@gmail.com>
2017-10-18 16:17:46 +0200

    Ajout fonction

--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -4,16 +4,22 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 
+import java.awt.Image;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ImageViewer extends JFrame /*implements ActionListener*/
 {
 	private DisplayedImage inputImage = new DisplayedImage(); 
 	private DisplayedImage ouputImage = new DisplayedImage();
-	private JButton buttonAction = new JButton("Action");
+	private JButton buttonInverse = new JButton("Inverse");
 
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu fileMenu = new JMenu("File");
@@ -29,11 +35,35 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		input.setLayout(new BoxLayout(input, BoxLayout.PAGE_AXIS));
 		input.add(inputImage);
 
-		JPanel action = new JPanel();
-		action.setLayout(new BoxLayout(action, BoxLayout.PAGE_AXIS));
-		action.add(buttonAction);
+		JPanel inverse = new JPanel();
+		inverse.setLayout(new BoxLayout(inverse, BoxLayout.PAGE_AXIS));
+		inverse.add(buttonInverse);
 		// Defines action associated to buttons
-		buttonAction.addActionListener(new ButtonListener());
+		
+		buttonInverse.addActionListener(new ButtonListener(){
+			public void actionPerformed(ActionEvent arg0) {						
+	    		BufferedImage Invimage;
+				Invimage = ouputImage.getimage();
+				
+				/*Début du traitement de l'image*/
+				int x,y;
+				int color;
+				
+				for(x=0;x<Invimage.getWidth();x++){
+					for(y=0; y<Invimage.getHeight();y++){
+						
+						color=Invimage.getRGB(x, y);
+						int blue = color & 0xff;
+						int green = (color & 0xff00) >> 8;
+						int red = (color & 0xff0000) >> 16;
+						
+						Color newclr= new Color(255-red, 255-green, 255-blue);
+						Invimage.setRGB(x, y, newclr.getRGB());
+						ouputImage.RefreshImage(Invimage);
+					}
+				}
+	    			
+	    		}});
 
 		JPanel output = new JPanel();
 		output.setLayout(new BoxLayout(output, BoxLayout.PAGE_AXIS));
@@ -42,7 +72,7 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		JPanel global = new JPanel();
 		global.setLayout(new BoxLayout(global, BoxLayout.LINE_AXIS));
 		global.add(input);
-		global.add(action);
+		global.add(inverse);
 		global.add(output);
 
 		this.getContentPane().add(global);
@@ -59,6 +89,8 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		this.setJMenuBar(menuBar);
 
 		this.setVisible(true);
+		
+		
 	}
 
 	/**
@@ -67,15 +99,7 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 	class ButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent arg0) 
 		{
-			DisplayedInvertedImage ouputImage2 = new DisplayedInvertedImage();
-			
-			JPanel output2 = new JPanel();
-			output2.setLayout(new BoxLayout(output2, BoxLayout.PAGE_AXIS));
-			output2.add(ouputImage2); 
-
-			JPanel global = new JPanel();
-			global.setLayout(new BoxLayout(global, BoxLayout.LINE_AXIS));
-			global.add(output2);
 		}
+		
 	}
 }
