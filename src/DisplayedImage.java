5b32b3c7b837bbad17e234e2154b11ec1259a67e
Fenouille <Fanny@Fenouille.com>
2017-10-25 23:21:50 +0200

    dernieres modifs pour que ça fonctionne

--- src/DisplayedImage.java
+++ src/DisplayedImage.java
@@ -4,17 +4,29 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
- 
+
 public class DisplayedImage extends JPanel {
 	
     private BufferedImage image;
     
+    
     public DisplayedImage() {
     		try {
     			image = ImageIO.read(new File("img.png"));
-        	} catch (IOException e) {
+           		} 
+    		catch (IOException e) {
         		e.printStackTrace();
         	}                
+    }
+    
+    static void setImage(BufferedImage img, String str){
+    	try {
+			img = ImageIO.read(new File(str));
+			repaint();
+       		} 
+		catch (IOException e) {
+    		e.printStackTrace();
+		}
     }
     
     public void paintComponent(Graphics g){
@@ -22,3 +34,5 @@ public class DisplayedImage extends JPanel {
     		g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this); // draw full image
     }                   
 }
+
+
--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -3,11 +3,17 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ImageViewer extends JFrame /*implements ActionListener*/
 {
@@ -17,7 +23,8 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu fileMenu = new JMenu("File");
-
+	
+	private JMenuItem itemCharge = new JMenuItem("Charger une image");
 	private JMenuItem itemClose = new JMenuItem("Close");
 
 	public ImageViewer () {
@@ -34,6 +41,12 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		action.add(buttonAction);
 		// Defines action associated to buttons
 		buttonAction.addActionListener(new ButtonListener());
+		
+		JPanel charger = new JPanel();
+		charger.setLayout(new BoxLayout(charger, BoxLayout.PAGE_AXIS));
+		charger.add(itemCharge);
+		// Defines action associated to buttons
+		itemCharge.addActionListener(new ButtonCharger());
 
 		JPanel output = new JPanel();
 		output.setLayout(new BoxLayout(output, BoxLayout.PAGE_AXIS));
@@ -53,7 +66,8 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 				System.exit(0);
 			}        
 		});
-		this.fileMenu.add(itemClose);  
+		this.fileMenu.add(itemClose); 
+		this.fileMenu.add(itemCharge);
 
 		this.menuBar.add(fileMenu);
 		this.setJMenuBar(menuBar);
@@ -70,4 +84,31 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 			System.out.println("Action Performed");
 		}
 	}
+	
+	class ButtonCharger implements ActionListener{
+		public JTextField status = new JTextField("pas de fichier chargé");
+	    public JFileChooser choose = new JFileChooser();
+	    private BufferedImage image;
+	    
+	    public void actionPerformed(ActionEvent evt){ //ouvrir une boite de dialogue pour choisir un fichier
+	    	choose.setApproveButtonText("Charger un fichier");
+	    	choose.showOpenDialog(null);
+	    	if (choose.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+	    		status.setText(choose.getSelectedFile().getAbsolutePath());
+	    		System.out.println(status.getText());
+	    	}
+	    	String str = status.getText(); // charger image 
+	    	try {
+    			DisplayedImage.setImage(image,str); 
+    			repaint();
+           		} 
+    		catch (IOException e) {
+        		e.printStackTrace();
+        	} 
+	    }
+	    
+	    
+	}
+	
+
 }
