4579a9461c9eec08f1a9195627abdc13cc2ad8a7
Fanouille <fanny.digues@depinfonancy.net>
2017-11-15 14:11:07 +0100

    Update KdNode.java
    ajout fonction removepointnode

--- src/KdNode.java
+++ src/KdNode.java
@@ -85,6 +85,90 @@ public class KdNode {
 			
 		}
 	}
+	
+	public int getNombreFils(){
+		if(this.filsG != null && this.filsD != null){
+			return 2;
+		}
+		else if (this.filsG != null || this.filsD != null){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+	
+	public KdNode getFilsG(){
+		return this.filsG;
+	}
+	
+	public KdNode getFilsD(){
+		return this.filsD;
+	}
+	
+	public void replaceNode(KdNode point){ //remplace this par point
+		this.coul = point.getcoul();
+		this.filsG = point.getFilsG();
+		this.filsD = point.getFilsD();
+	}
+	
+	
+	public ArrayList<KdNode> getPointFromTree(KdNode point, ArrayList<KdNode> liste){ //donne tous les points en dessous dans l'arbre
+		if (point != null){
+ 			liste.add(point);
+			if(point.getFilsG()!=null){
+				getPointFromTree(point.getFilsG(),liste);
+			}
+			else if(point.getFilsD()!=null){
+				getPointFromTree(point.getFilsD(),liste);
+			}
+		}
+		return liste;
+	}
+	
+	public int getDirection(){
+		return this.P % 3; //0:R;1:V;2:B
+	}
+	
+	public int removePointNode(KdNode point){ // enleve point et reconstruit arbre, renvoie profondeur 
+		int direction = this.getDirection();
+		int new_p = 0;
+		switch (this.coul.comparaison(direction, point.getcoul())){
+		case 1: //fils de gauche
+			return (this.filsG.removePointNode(point));
+		
+		case -1: //fils de droite
+			return (this.filsD.removePointNode(point));
+			
+		default: //0 
+			if (this.coul.egal(point.getcoul())){//c'est le point à supprimer
+				//son fils de droite va prendre sa place, tous les fils de gauche reconstruits
+				point.replaceNode(point.getFilsD());//il emmene tous ses fils avec 
+				
+				ArrayList<KdNode> liste = new ArrayList<KdNode>();
+				getPointFromTree(point.getFilsG(), liste);
+				for (int i=0;i<liste.size();i++){
+					new_p = point.getFilsD().addpointnode(liste.get(i).getcoul()); // point fils ou point tt court
+				}
+				//calcul de la nouvelle profondeur
+				if (new_p<KdTree.getProf()){
+					return new_p;
+				}
+				else{
+					return KdTree.getProf();
+				}
+			}
+			
+			else{// c'est un autre point on va à gauche
+				return (this.filsG.removePointNode(point));
+			}
+			
+			
+			
+		}
+	}
+	
+	
 	public Couleur Moy()
 	{
 		if(fils[0] && fils[1])
